798b2a9d64c6fa4f9acbdf97ef1640e3e6a7022f
cwseitz <cwseitz2@gmail.com>
2022-09-08 18:39:28 -0600

    update

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup = setup(
+    name='stmcmc',
+    description='Sequential tempered Markov Chain Monte Carlo in Python',
+    version='1.0',
+    packages=find_packages(),
+)
